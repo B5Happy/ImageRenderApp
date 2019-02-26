2d9fd4d7ccb8d4c24c38aedecb42f5d3a880f4f9
Saly <saly@juke4me.com>
2019-02-26 22:59:02 +0530

    add the button

---
+++ lib/src/app.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
+
+        ),
+        appBar: AppBar(
+          title: Text('Lets see some images!'),
+
+
+        ),
+      )
+    );
+  }
+}
